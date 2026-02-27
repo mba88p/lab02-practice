8ee59e325a03063eddcedc10e64f4ef46591131d
Muhammad bin Ahsan <muhammadbinahsan708@gmail.com>
2026-02-27 15:16:15 +0500

    Update math tools with unit tests

--- mathtools.py
+++ mathtools.py
@@ -8,3 +8,38 @@ def multiply(a,b):
 
 def is_even(n):
     return n % 2 == 0
+
+def subtract(a, b):
+    return a - b
+
+def max_of_three(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+    
+def is_palindrome(a):
+    sanitized = a.replace(" ", "")
+    return sanitized == sanitized[::-1]
+
+def find_min(numbers):
+    if not numbers:
+        return None
+    
+    min_val = numbers[0]
+    
+    for num in numbers:
+        if num < min_val:
+            min_val = num
+    return min_val
+
+def remove_duplicates(items):
+    result = []
+    
+    for item in items:
+        if item not in result:
+            result.append(item)
+            
+    return result
--- test_math_tools.py
+++ test_math_tools.py
@@ -1,5 +1,5 @@
 # test_math_tools.py
-from mathtools import add, multiply, is_even
+from mathtools import add, multiply, is_even, subtract, max_of_three, is_palindrome, find_min, remove_duplicates
 
 def test_add():
     assert add(2, 3) == 5
@@ -19,8 +19,53 @@ def test_is_even():
     assert is_even (0) == True
     print (" test_is_even : ALL PASSED ")
 
+def test_subtract():
+    assert subtract(5, 4) == 1
+    assert subtract(1, -2) == 3
+    assert subtract(0, 0) == 0
+    
+def test_max_of_three():
+    assert max_of_three(0, 1, 2) == 2
+    assert max_of_three(0, 3, 1) == 3
+    assert max_of_three(4, 2, 1) == 4
+    assert max_of_three(4, 4, 5) == 5
+    assert max_of_three(3, 3, 3) == 3
+
+def test_is_palindrome():
+    assert is_palindrome("race car") == True 
+    assert is_palindrome("Race car") == False
+    assert is_palindrome("12321") == True
+    assert is_palindrome("apple") == False
+    assert is_palindrome("step on no pets") == True
+    assert is_palindrome("  ") == True
+    assert is_palindrome("") == True
+    assert is_palindrome("a") == True
+
+def test_find_min():
+    assert find_min([5, 10, 15]) == 5
+    assert find_min([-10, -20, -5]) == -20
+    assert find_min([-1, 0, 1]) == -1
+    assert find_min([42]) == 42
+    assert find_min([7, 2, 2, 8]) == 2
+    assert find_min([]) is None
+
+def test_remove_duplicates():
+    assert remove_duplicates([]) == []
+    assert remove_duplicates([10, 20, 30]) == [10, 20, 30]
+    assert remove_duplicates(["a", "a", "a"]) == ["a"]
+    assert remove_duplicates([1, "1", 2]) == [1, "1", 2]
+    assert remove_duplicates([5, 1, 5, 2, 1]) == [5, 1, 2]
+    nested = [[1, 2], [1, 2], [3]]
+    assert remove_duplicates(nested) == [[1, 2], [3]]
+    
 # Run all tests
 test_add()
+test_subtract()
 test_multiply()
 test_is_even()
+test_max_of_three()
+test_is_palindrome()
+test_find_min()
+test_remove_duplicates()
+
 print (" --- All tests passed ! ---")
